86fbc3ec999d3359f07d2d871e6551f60d7a0217
anchoriteFili <feiliyaailiya1@ilcoud.com>
2017-12-21 09:53:31 +0800

    先停一下，搞搞微信登录

--- RxSwiftTest/RxSwiftTest/ViewController.swift
+++ RxSwiftTest/RxSwiftTest/ViewController.swift
@@ -7,12 +7,54 @@
 //
 
 import UIKit
-
+import RxSwift
 class ViewController: UIViewController {
+    
+    var subscription: Disposable?
+    
+    
+    
+    @IBOutlet weak var textfield: UITextField!
+    
+    
+    @IBAction func cancelObserve(_ sender: UIButton) {
+        
+        
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        let scheduler = SerialDispatchQueueScheduler(qos: .default)
+        let subscription = Observable<Int>.interval(0.3, scheduler: scheduler)
+            .subscribe { (event) in
+                print(event)
+        }
+        
+//        Thread.sleep(forTimeInterval: 5.0)
+        subscription.dispose() // 此代码可以停止上面的定时器
+        
+        
+        let array = [1,2,3,4,5]
+        
+        let array2 = array.filter({$0 > 1}).map({$0 * 2}) // 4 6 8 10
+        var indexGenerator = array2.makeIterator()
+        print(indexGenerator)
+        let first = indexGenerator.next()!
+        let second = indexGenerator.next()!
+        print("\(String(describing: first)) ******** \(String(describing: second))")
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
